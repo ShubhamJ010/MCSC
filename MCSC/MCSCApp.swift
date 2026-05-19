52b0181764335918aeb3156dee3a4e711711048e
Shubham Jha <samxn81@gmail.com>
2026-05-20 00:44:43 +0530

    Initial Commit

---
+++ MCSC/MCSCApp.swift
@@ -0,0 +1,17 @@
+//
+//  MCSCApp.swift
+//  MCSC
+//
+//  Created by Shubham Jha on 20/05/26.
+//
+
+import SwiftUI
+
+@main
+struct MCSCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
